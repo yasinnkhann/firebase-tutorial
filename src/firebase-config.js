10bd0a2ee2b9dd8e0c9909951151ba969947a9d9
Yasin Khan <yasinkhan500@gmail.com>
2021-10-05 14:31:03 -0700

    restarted

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2aTmWeCBbwHrA-sP-8pRM5ToT7BxS_LU",
-  authDomain: "fir-tutorial-87432.firebaseapp.com",
-  projectId: "fir-tutorial-87432",
-  storageBucket: "fir-tutorial-87432.appspot.com",
-  messagingSenderId: "684136126631",
-  appId: "1:684136126631:web:f145797b89377c0f232382",
-  measurementId: "G-DK15XFGLS0"
+  apiKey: "AIzaSyCaircZlkZgXq4fQz9c1k55o5zp1FQgCYw",
+  authDomain: "fir-tutorial-7f620.firebaseapp.com",
+  projectId: "fir-tutorial-7f620",
+  storageBucket: "fir-tutorial-7f620.appspot.com",
+  messagingSenderId: "93942516749",
+  appId: "1:93942516749:web:34d6bb5f243b368b401944",
+  measurementId: "G-LMNJMFRWJZ"
 };
 
 const app = initializeApp(firebaseConfig);
